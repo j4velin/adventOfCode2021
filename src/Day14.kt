500b6d91e2f22638e05e63e215b0f4442d3987ab
Thomas Hoffmann <git@j4velin.de>
2021-12-15 08:35:56 +0100

    day 14 part 2 solution

--- src/Day14.kt
+++ src/Day14.kt
@@ -3,36 +3,83 @@
  * @param rules a map of polymer transformation rules. Key=Input elements, Value=Element to be inserted between the input elements
  * @return a polymer string constructed by taking the [input] string an applying all the [rules] simultaneously
  */
-private fun applyRules(input: String, rules: Map<String, String>) =
-    input.windowed(2).map { it[0] + rules.getOrDefault(it, it) + it[1] }.foldIndexed(StringBuilder()) { index, acc, s ->
-        if (index == 0) acc.append(s) else acc.append(s.drop(1))
-    }.toString()
+private fun applyRules(input: String, rules: Map<String, Char>) =
+    input.windowed(2).map { it[0] + rules.getOrDefault(it, it).toString() + it[1] }
+        .foldIndexed(StringBuilder()) { index, acc, s ->
+            if (index == 0) acc.append(s) else acc.append(s.drop(1))
+        }.toString()
 
 /**
  * Parses the input for to a map of polymer transformation rules
  */
-private fun parseRules(input: List<String>) = input.drop(2).map { it.split(" -> ") }.associate { Pair(it[0], it[1]) }
+private fun parseRules(input: List<String>) =
+    input.drop(2).map { it.split(" -> ") }.associate { Pair(it[0], it[1].first()) }
 
-private fun calculateResult(resultPolymer: String): Long {
-    val occurrences = resultPolymer.toCharArray().groupBy { it }.mapValues { it.value.size }
-    return occurrences.values.maxOf { it.toLong() } - occurrences.values.minOf { it.toLong() }
-}
+/**
+ * Calculates the result number for part1 & part2 based on the occurrences of each character in the final polymer string
+ */
+private fun calculateResult(occurrences: Map<Char, Long>) =
+    occurrences.values.maxOf { it } - occurrences.values.minOf { it }
 
 private fun part1(input: List<String>): Int {
     var current = input.first()
     val rules = parseRules(input)
     repeat(10) { current = applyRules(current, rules) }
-    return calculateResult(current).toInt()
+    val occurrences = current.toCharArray().groupBy { it }.mapValues { it.value.size.toLong() }
+    return calculateResult(occurrences).toInt()
+}
+
+/**
+ * A polymer factory to generate a polymer string based on an initial polymer and a set of transformation rules.
+ *
+ * @param input the initial polymer string
+ * @property rules the transformation rules. Key=Pair of characters, Value=Character to place in between the Key in a single step
+ */
+private class PolymerFactory(input: String, private val rules: Map<String, Char>) {
+    private val lastCharacter = input.last()
+    private val occurrences: MutableMap<String, Long> = HashMap(26 * 26)
+
+    init {
+        input.windowed(2).forEach { occurrences[it] = (occurrences[it] ?: 0L) + 1 }
+    }
+
+    /**
+     * Performs one building step, e.g. applies all the rules once
+     */
+    fun performBuildStep() {
+        val diff: MutableMap<String, Long> = HashMap(rules.size)
+        for (rule in rules) {
+            val currentCount = occurrences[rule.key] ?: 0L
+            if (currentCount > 0L) {
+                diff[rule.key] = (diff[rule.key] ?: 0L) - currentCount
+                val newPair1 = String(charArrayOf(rule.key[0], rule.value))
+                val newPair2 = String(charArrayOf(rule.value, rule.key[1]))
+                diff[newPair1] = (diff[newPair1] ?: 0L) + currentCount
+                diff[newPair2] = (diff[newPair2] ?: 0L) + currentCount
+            }
+        }
+        diff.forEach { occurrences[it.key] = (occurrences[it.key] ?: 0L) + it.value }
+    }
+
+    /**
+     * @return the number of occurrences of each character in the current polymer
+     */
+    fun getOccurrences(): Map<Char, Long> {
+        val result: MutableMap<Char, Long> = HashMap(26)
+        occurrences.forEach { result[it.key.first()] = (result[it.key.first()] ?: 0L) + it.value }
+        result[lastCharacter] = (result[lastCharacter] ?: 0) + 1
+        return result
+    }
+
 }
 
 private fun part2(input: List<String>): Long {
-    var current = input.first()
     val rules = parseRules(input)
+    val factory = PolymerFactory(input.first(), rules)
     repeat(40) {
-        current = applyRules(current, rules)
-        println("$it - "+current.length)
+        factory.performBuildStep()
     }
-    return calculateResult(current)
+    return calculateResult(factory.getOccurrences())
 }
 
 fun main() {
